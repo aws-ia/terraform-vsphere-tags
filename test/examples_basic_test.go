55ed727ed9fb7e9b6cb631e4ec3d61b81af89c7a
Troy Lindsay <tlindsay42@users.noreply.github.com>
2022-03-14 11:33:24 -0500

    Add import tests

--- test/examples_basic_test.go
+++ test/examples_basic_test.go
@@ -10,28 +10,23 @@ import (
 
 // You must set these environment variables for this test
 const (
-	env_var_not_set_error_message = "Environment variable: '%s' is not set."
-	vsphere_user                  = "VSPHERE_USER"
-	vsphere_password              = "VSPHERE_PASSWORD"
-	vsphere_server                = "VSPHERE_SERVER"
+	vsphere_user     = "VSPHERE_USER"
+	vsphere_password = "VSPHERE_PASSWORD"
+	vsphere_server   = "VSPHERE_SERVER"
 	// vsphere_allow_unverified_ssl = "VSPHERE_ALLOW_UNVERIFIED_SSL"
 )
 
-func TestExamplesBasic(t *testing.T) {
-	if os.Getenv(vsphere_user) == "" {
-		fmt.Println("Environment variable: '" + vsphere_user + "' is not set.")
+func GetEnvOrExit(env_var string) {
+	if os.Getenv(env_var) == "" {
+		fmt.Println("Environment variable: '" + env_var + "' is not set.")
 		os.Exit(1)
 	}
+}
 
-	if os.Getenv(vsphere_password) == "" {
-		fmt.Println("Environment variable: '" + vsphere_password + "' is not set.")
-		os.Exit(1)
-	}
-
-	if os.Getenv(vsphere_server) == "" {
-		fmt.Println("Environment variable: '" + vsphere_server + "' is not set.")
-		os.Exit(1)
-	}
+func TestExamplesNewCategoryNewTags(t *testing.T) {
+	GetEnvOrExit(vsphere_user)
+	GetEnvOrExit(vsphere_password)
+	GetEnvOrExit(vsphere_server)
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/basic",
@@ -39,6 +34,44 @@ func TestExamplesBasic(t *testing.T) {
 		// 	"myvar":     "test",
 		// 	"mylistvar": []string{"list_item_1"},
 		// },
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+}
+
+func TestExamplesImportCategoryImportTag(t *testing.T) {
+	GetEnvOrExit(vsphere_user)
+	GetEnvOrExit(vsphere_password)
+	GetEnvOrExit(vsphere_server)
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples/basic",
+		Vars: map[string]interface{}{
+			"vsphere_tag_category_name":   "terraform",
+			"vsphere_tags":                []interface{}{map[string]interface{}{"name": "terraform", "description": ""}},
+			"create_vsphere_tag_category": false,
+			"create_vsphere_tags":         false,
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+}
+
+func TestExamplesImportCategoryNewTag(t *testing.T) {
+	GetEnvOrExit(vsphere_user)
+	GetEnvOrExit(vsphere_password)
+	GetEnvOrExit(vsphere_server)
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples/basic",
+		Vars: map[string]interface{}{
+			"vsphere_tag_category_name":   "terraform",
+			"vsphere_tags":                []interface{}{map[string]interface{}{"name": "project", "description": "terraform-vsphere-tags"}},
+			"create_vsphere_tag_category": false,
+			"create_vsphere_tags":         true,
+		},
 	}
 
 	defer terraform.Destroy(t, terraformOptions)
